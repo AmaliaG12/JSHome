09f56ba63fe11ebcc416d6ffbdc68e7e2832ce2c
AmaliaG12 <luisagutierrez12082004@gmail.com>
2024-07-31 20:31:31 -0600

    se suben los archivos

---
+++ 8/ejemplos.js
@@ -0,0 +1,25 @@
+let numero = [1,2,3]
+let frutas = ['melon','sandia','pera', numero]
+console.log(frutas);
+
+let numero1 = [1,2,3]
+let frutass = ['melon','sandia','pera', numero1]
+console.log(frutass[3]);
+
+let fruits = ['sandia','melon','durazno','uva']
+fruits.pop();
+console.log(fruits);
+
+let fruta = ['sandia','melon','pera','uva']
+fruta.push('manzana');
+console.log(fruta);
+fruta.push('papaya');
+console.log(fruta);
+
+let carro = ['sandia','audi','toyota','mercedes','mitsubishi']
+carro.shift();
+console.log(carro);
+
+let carro1 = ['sandia','audi','toyota','mercedes','porche']
+carro1.unshift('ferrari')
+console.log(carro1);
---
+++ 8/ejercicio1.js
@@ -0,0 +1,13 @@
+let numbers = [1,2,3,4,5]
+numbers.push(6);
+console.log(numbers);
+numbers.shift();
+console.log(numbers);
+numbers.unshift(1)
+console.log(numbers);
+console.log(numbers[0]);
+console.log(numbers[1]);
+console.log(numbers[2]);
+console.log(numbers[3]);
+console.log(numbers[4]);
+console.log(numbers[5]);
